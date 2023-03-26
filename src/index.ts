0499d57a0a84183bca1544c049a00152d9a4f902
taisei0123 <taisei8955@gmail.com>
2023-03-26 16:20:03 +0900

    Allows the Open AI API to response for a question
    Uses the Open AI API and gpt 3.5 turbo model
    Adds an ENV for the open api key
    Adds fucntion to ask the chatGPT a question

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello, World!!");
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from "dotenv";
+import { gptModel } from "./utils/constants";
+
+const ask = async (openai: OpenAIApi, content: string) => {
+  const response = await openai.createChatCompletion({
+    model: gptModel,
+    messages: [{ role: "user", content: content }],
+  });
+
+  const answer = response.data.choices[0].message?.content;
+  console.log(answer);
+};
+
+const main = () => {
+  dotenv.config();
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const question = "Can you tell me what chatGPT is";
+  ask(openai, question);
+};
+
+main();
